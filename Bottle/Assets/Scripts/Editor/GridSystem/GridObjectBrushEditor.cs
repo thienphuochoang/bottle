c4a7758f700940b6eb9ff4c98851b187f1e353b1
Phuoc Hoang <htphuoc1995@gmail.com>
2022-04-19 22:52:35 +0700

    update box fill and paint cell function

--- Bottle/Assets/Scripts/Editor/GridSystem/GridObjectBrushEditor.cs
+++ Bottle/Assets/Scripts/Editor/GridSystem/GridObjectBrushEditor.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEditor;
 using UnityEditor.Tilemaps;
 using Bottle.Core.GridObjectData;
+using System;
 namespace Bottle.Editor.GridSystem
 {
     [System.Serializable]
@@ -84,6 +85,36 @@ namespace Bottle.Editor.GridSystem
     [CustomGridBrush(true, false, false, "[Bottle] Grid Object Brush")]
     public class GridObjectBrushEditor : GridBrushBase
     {
+        public int sizeCount
+        {
+            get { return _size.x * _size.y * _size.z; }
+        }
+        private bool _isCellSelected = false;
+        public bool IsCellSelected
+        {
+            get
+            {
+                if (_isCellSelected == true && IsCellNull == false)
+                    return true;
+                return false;
+            }
+            set
+            {
+                _isCellSelected = value;
+            }
+        }
+        public bool IsCellNull
+        {
+            get
+            {
+                foreach (var eachCell in _cells)
+                {
+                    if (eachCell != null)
+                        return false;
+                }
+                return true;
+            }
+        }
         private BrushCell _currentSelectedBrushCell;
         [SerializeField]
         private BrushCell[] _cells;
@@ -103,6 +134,17 @@ namespace Bottle.Editor.GridSystem
             }
         }
 
+        public GridObjectBrushEditor()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            _pivot = Vector3Int.zero;
+            _size = Vector3Int.one;
+            SizeUpdated();
+        }
 
         public int GetCellIndexWrapAround(int x, int y, int z)
         {
@@ -122,15 +164,28 @@ namespace Bottle.Editor.GridSystem
             foreach (Vector3Int location in position.allPositionsWithin)
             {
                 Vector3Int local = location - position.min;
-                BrushCell tile = _cells[GetCellIndexWrapAround(local.x, local.y, local.z)];
+                BrushCell tile = cells[GetCellIndexWrapAround(local.x, local.y, local.z)];
                 if (tile != null)
                     PaintCell(gridLayout, location, tile);
             }
         }
 
+        public void DeselectCell()
+        {
+            //Re-init
+            Init();
+            IsCellSelected = false;
+
+            if (_currentSelectedBrushCell != null)
+                _cells[0] = _currentSelectedBrushCell;
+        }
+
         public override void Paint(GridLayout gridLayout, GameObject brushTarget, Vector3Int position)
         {
             if (brushTarget == null || gridLayout == null) return;
+            if (IsCellSelected == false)
+                DeselectCell();
+
             Vector3Int min = position - pivot;
             BoundsInt bounds = new BoundsInt(min, _size);
             BoxFill(gridLayout, brushTarget, bounds);
@@ -142,6 +197,27 @@ namespace Bottle.Editor.GridSystem
             _currentSelectedBrushCell.Tile = gridTile;
             _currentSelectedBrushCell.Scale = scale;
             _currentSelectedBrushCell.Rotation = orientation;
+        }
+        public int GetCellIndex(Vector3Int brushPosition)
+        {
+            return GetCellIndex(brushPosition.x, brushPosition.y, brushPosition.z);
+        }
+
+        public int GetCellIndex(int x, int y, int z)
+        {
+            return x + _size.x * y + _size.x * _size.y * z;
+        }
+
+        private void SizeUpdated()
+        {
+            Array.Resize(ref _cells, sizeCount);
+            _cells = new BrushCell[_size.x * _size.y * _size.z];
+            BoundsInt bounds = new BoundsInt(Vector3Int.zero, _size);
+            foreach (Vector3Int pos in bounds.allPositionsWithin)
+            {
+                if (_cells[GetCellIndex(pos)] == null)
+                    _cells[GetCellIndex(pos)] = new BrushCell();
+            }
         }
     }
 }
