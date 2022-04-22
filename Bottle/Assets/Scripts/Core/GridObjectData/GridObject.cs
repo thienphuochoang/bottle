d7e546bbcef67fd380fe8131015369c9c6885f75
PhuocHoang <htphuoc1995@gmail.com>
2022-04-22 14:32:33 +0700

    fix locked-y axis movement in grid

--- Bottle/Assets/Scripts/Core/GridObjectData/GridObject.cs
+++ Bottle/Assets/Scripts/Core/GridObjectData/GridObject.cs
@@ -9,7 +9,8 @@ namespace Bottle.Core.GridObjectData
     [ExecuteInEditMode]
     public class GridObject : MonoBehaviour
     {
-        private float threshold = 0.2f;
+        [SerializeField]
+        private Vector3 lastPos;
 
         [ReadOnly]
         [BoxGroup("Grid Object Settings")]
@@ -21,12 +22,23 @@ namespace Bottle.Core.GridObjectData
         [Tooltip("The height of this grid object in the Grid.")]
         public float gridHeight = 0f;
 
+        private void Awake()
+        {
+            
+        }
+
         void Update()
         {
-            Vector3Int cellPosition = GridManager.Instance.grid.WorldToCell(this.transform.position);
-            Vector3 newPos = GridManager.Instance.grid.GetCellCenterWorld(cellPosition);
-            this.transform.position = newPos;
-            //gridPosition = new Vector2Int(Mathf.RoundToInt(newPos.x), Mathf.RoundToInt(newPos.z));
+            if (this.transform.hasChanged)
+            {
+                Vector3Int cellPosition = GridManager.Instance.grid.WorldToCell(this.transform.position);
+                Vector3 newPos = GridManager.Instance.grid.GetCellCenterWorld(cellPosition);
+                Debug.Log(newPos);
+                newPos.y = Mathf.Ceil(this.transform.position.y);
+                this.transform.position = newPos;
+                gridPosition = new Vector2Int(Mathf.RoundToInt(newPos.x), Mathf.RoundToInt(newPos.z));
+                gridHeight = Mathf.RoundToInt(newPos.y);
+            }
         }
     }
 }
