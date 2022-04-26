a7798d8eb3b80c2d33554dc772c1e9f23b003321
Phuoc Hoang <htphuoc1995@gmail.com>
2022-04-26 23:49:33 +0700

    fix pivot of grid tile, save turn function

--- Bottle/Assets/Scripts/Core/Manager/GameplayManager.cs
+++ Bottle/Assets/Scripts/Core/Manager/GameplayManager.cs
@@ -3,14 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using Bottle.Extensions.Singleton;
 using Bottle.Core.GridObjectData;
+using Sirenix.OdinInspector;
 namespace Bottle.Core.Manager
 {
+    public class GridObjectSavedData
+    {
+        public GameObject gridGameObject;
+        public GridTile gridTile;
+        public GridEntity gridEntity;
+
+        public GridObjectSavedData() { }
+        public GridObjectSavedData(GridObjectSavedData gridObjectSavedData)
+        {
+            this.gridGameObject = gridObjectSavedData.gridGameObject;
+            this.gridTile = gridObjectSavedData.gridTile;
+            this.gridEntity = gridObjectSavedData.gridEntity;
+        }
+    }
+
     public class GameplayManager : PersistentObject<GameplayManager>
     {
-        private int turn;
+        [ValueDropdown("_turns")]
+        public int currentTurn;
+
+
+        private List<int> _turns = new List<int>();
+        public List<int> Turns
+        {
+            get
+            {
+                return _turns;
+            }
+            set
+            {
+                _turns = value;
+            }
+        }
+        [SerializeField]
+        private List<GridObjectSavedData> gridObjectSavedDatas = new List<GridObjectSavedData>();
         public void SaveSceneState()
         {
             GridTile[] allGridTiles = GridManager.Instance.TileHolder.GetComponentsInChildren<GridTile>();
+            foreach (GridTile gridTile in allGridTiles)
+            {
+                GridObjectSavedData currentGridObjectData = new GridObjectSavedData();
+                currentGridObjectData.gridGameObject = gridTile.gameObject;
+                currentGridObjectData.gridTile = gridTile;
+                currentGridObjectData.gridEntity = null;
+            }
             GridEntity[] allGridEntities = GridManager.Instance.EntityHolder.GetComponentsInChildren<GridEntity>();
         }
 
